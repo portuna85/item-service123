42b1c7985e26bce9f4d8636659353c393c022615
portuna85 <portuna85@gmail.com>
2022-03-11 21:10:07 +0900

    item-service UPDATE

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,16 +1,18 @@
 package hello.itemservice.domain.item;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class ItemRepository {
-    private static final Map<Long, Item> store = new HashMap<>(); //static 사용
-    private static long sequence = 0L; //static 사용
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
 
     public Item save(Item item) {
-        item.setId(++sequence);
         store.put(item.getId(), item);
         return item;
     }

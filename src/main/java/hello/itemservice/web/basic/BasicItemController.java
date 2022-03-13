ceeac9134b5c6125dd5b06e143e7548a80a1b2da
portuna85 <portuna85@gmail.com>
2022-03-13 11:01:35 +0900

    item-service UPDATE

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -14,7 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
-public class BasicController {
+public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
@@ -26,9 +27,19 @@ public class BasicController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
     @PostConstruct
     public void init() {
-        itemRepository.save(new Item("ItemA",10000, 10));
-        itemRepository.save(new Item("ItemB",20000, 20));
+        itemRepository.save(new Item("ItemA", 10000, 10));
+        itemRepository.save(new Item("ItemB", 20000, 20));
     }
 }

d2673d721f96c36fcf91ff787fecb0c2a0250998
portuna85 <portuna85@gmail.com>
2022-03-13 14:31:00 +0900

    item-service UPDATE

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -37,8 +37,8 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save(@RequestParam String itemName,
+    // @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
                        @RequestParam int price,
                        @RequestParam Integer quantity,
                        Model model) {
@@ -54,31 +54,35 @@ public class BasicItemController {
     }
 
     // @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                            @RequestParam int price,
-                            @RequestParam Integer quantity,
-                            Model model) {
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-        itemRepository.save(item);
-        model.addAttribute("item", item);
-        return "basic/item";
-    }
-
-    /**
-     * @ModelAttribute("item") Item item
-     * model.addAttribute("item", item); 자동 추가
-     */
-    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
         itemRepository.save(item);
-        // model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        // model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute() Item item, Model model) {
+
+        itemRepository.save(item);
+
+        // model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(Item item) {
+
+        itemRepository.save(item);
+
         return "basic/item";
     }
 
     /**
+
+
+     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
